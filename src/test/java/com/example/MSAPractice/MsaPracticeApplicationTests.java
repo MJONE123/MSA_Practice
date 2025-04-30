908562e1fe92984da8e723a5bf2212c1cefae276
문준일 <answnsdlfwkd@naver.com>
2025-04-30 18:02:25 +0900

    ❇️ feat: MSA 구조 프로젝트 생성

---
+++ src/test/java/com/example/MSAPractice/MsaPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MSAPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsaPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
